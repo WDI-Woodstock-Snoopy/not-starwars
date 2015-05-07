bc264914247d98a90e085cd849c00dc192df4ece
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2015-05-07 16:03:51 -0500

    model includes update method, need params tho

--- Not_Starwars/app/controllers/gangstas_controller.rb
+++ Not_Starwars/app/controllers/gangstas_controller.rb
@@ -17,10 +17,10 @@ class GangstasController < ApplicationController
 
   def edit
     @gangstum = Gangstum.find(params[:id])
-    @gangstum.update(params[:gangstum])
   end
   def update
-    #mightygaby
+    gangstum = Gangstum.find(params[:id])
+    # gangstum.update(params[])     <-- this needs params
   end
 
   def destroy
