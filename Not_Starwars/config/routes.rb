504c5818151b0c88e7f4ff99e1bf12b228eeb747
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2015-05-07 16:22:05 -0500

    index.erb maps over gangstas and displays ganstum

--- Not_Starwars/config/routes.rb
+++ Not_Starwars/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root '/' => 'gangstas#index' 
+  root 'gangstas#index'
 
   resources :gangstas
   # The priority is based upon order of creation: first created -> highest priority.
